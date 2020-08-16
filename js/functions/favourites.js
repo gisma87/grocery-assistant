fad255c42310f88073d3f16416178805bd9f1110
Kate <54095868+romcath@users.noreply.github.com>
2020-08-16 20:40:37 +0300

    fix: update js

--- js/functions/favourites.js
+++ js/functions/favourites.js
@@ -2,10 +2,15 @@
 function addFavouritesHandler(event) {
   const iconElement = event.target;
   const favouritesId = iconElement.getAttribute('data-id');
+  const tooltip = iconElement.querySelector('.button-star__tooltip');
 
   api.addFavourites(favouritesId).then(() => {
     iconElement.classList.remove('button-star_inactive');
     iconElement.classList.add('button-star_active');
+
+    if (tooltip != null) {
+      tooltip.setAttribute('style', 'display: none');
+    }
 
     iconElement.removeEventListener('click', addFavouritesHandler);
     iconElement.addEventListener('click', removeFavouritesHandler);
@@ -20,10 +25,16 @@ function addFavouritesHandler(event) {
 function removeFavouritesHandler(event) {
   const iconElement = event.target;
   const favouritesId = iconElement.getAttribute('data-id');
+  const tooltip = iconElement.querySelector('.button-star__tooltip');
 
   api.removeFavourites(favouritesId).then(() => {
     iconElement.classList.remove('button-star_active');
     iconElement.classList.add('button-star_inactive');
+
+    if (tooltip != null) {
+      tooltip.setAttribute('style', 'display: block');
+      tooltip.textContent = 'Добавить в избранное';
+    }
 
     iconElement.removeEventListener('click', removeFavouritesHandler);
     iconElement.addEventListener('click', addFavouritesHandler);
--- js/page-recipe-auth.js
+++ js/page-recipe-auth.js
@@ -10,8 +10,19 @@ const buttonStar = document.querySelector('.button-star');
 const buttonRecipe = document.querySelector('.recipe__button');
 const buttonAuthor = document.querySelector('.button_color_grey');
 
+// Добавляет слушатель на кнопки добавления в избранное
+function setListeners(element, event) {
+    if (element.classList.contains('button-star_active')) {
+      element.addEventListener(event, removeFavouritesHandler);
+    } else {
+      element.addEventListener(event, addFavouritesHandler);
+    }
+}
+
 // Добавляет слушатели
-buttonStar.addEventListener('click', addFavouritesHandler);
 buttonRecipe.addEventListener('click', addRecipeHandler);
 buttonAuthor.addEventListener('click', addSubscribtionHandler);
+
+// Вызов функций
+setListeners(buttonStar, 'click');
 
